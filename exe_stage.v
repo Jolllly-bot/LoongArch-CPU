410bee458d38abac0c61398c3fe90485dfe4aaaa
yanbei19 <yanbei19@mails.ucas.ac.cn>
2021-10-04 10:16:10 +0000

    Lab 6:add instructions

--- exe_stage.v
+++ exe_stage.v
@@ -36,9 +36,50 @@ wire [31:0] es_rj_value   ;
 wire [31:0] es_rkd_value  ;
 wire [31:0] es_pc         ;
 
+wire [63:0] unsigned_prod, signed_prod;
+reg signed_divisor_tvalid;
+wire signed_divisor_tready;
+wire [31:0] signed_divisor_tdata;
+reg signed_dividend_tvalid;
+wire signed_dividend_tready;
+wire [31:0] signed_dividend_tdata;
+
+reg unsigned_divisor_tvalid;
+wire unsigned_divisor_tready;
+wire [31:0] unsigned_divisor_tdata;
+reg unsigned_dividend_tvalid;
+wire unsigned_dividend_tready;
+wire [31:0] unsigned_dividend_tdata;
+
+wire [63:0] unsigned_div_result;
+
+
+wire [63:0] signed_div_result;
+
+wire signed_dout_tvalid;
+wire unsigned_dout_tvalid;
+
+assign signed_divisor_tdata = es_alu_src2;
+assign signed_dividend_tdata = es_alu_src1;
+
+
 wire        es_res_from_mem;
 
-assign {es_alu_op      ,  //149:138
+wire es_mul_signed;
+wire es_mul_unsigned;
+wire es_mul_high;
+wire es_div_signed;
+wire es_div_unsigned;
+wire es_div_mod;
+
+assign {es_mul_signed,
+        es_mul_unsigned,
+        es_mul_high,
+        es_div_signed,
+        es_div_unsigned,
+        es_div_mod,
+        es_src2_is_zimm,
+        es_alu_op      ,  //149:138
         es_res_from_mem,  //137:137
         es_src1_is_pc  ,  //136:136
         es_src2_is_imm ,  //135:135
@@ -54,12 +95,15 @@ assign {es_alu_op      ,  //149:138
 wire [31:0] es_alu_src1   ;
 wire [31:0] es_alu_src2   ;
 wire [31:0] es_alu_result ;
+wire [31:0] es_result ;
+reg [3:0] div_cycle;
+reg [3:0] divu_cycle;
 
 //assign es_res_from_mem = es_load_op;
 assign es_to_ms_bus = {es_res_from_mem,  //70:70
                        es_gr_we       ,  //69:69
                        es_dest        ,  //68:64
-                       es_alu_result  ,  //63:32
+                       es_result  ,  //63:32
                        es_pc             //31:0
                       };
 
@@ -75,7 +119,7 @@ assign es_fwd_bus = {es_fwd_valid ,  //38:38
                      es_alu_result  //31:0
                     };
 
-assign es_ready_go    = 1'b1;
+assign es_ready_go    = (~(es_div_signed | es_div_unsigned)) | (es_div_signed & signed_dout_tvalid) | (es_div_unsigned & unsigned_dout_tvalid) ;
 assign es_allowin     = !es_valid || es_ready_go && ms_allowin;
 assign es_to_ms_valid =  es_valid && es_ready_go;
 always @(posedge clk) begin
@@ -91,6 +135,14 @@ always @(posedge clk) begin
     end
 end
 
+assign es_result = (es_mul_signed && es_mul_high)  ? signed_prod[63:32] :
+                   (es_mul_signed && ~es_mul_high) ? signed_prod[31:0] :
+                   (es_mul_unsigned && es_mul_high)? unsigned_prod[63:32]:
+                   (es_div_signed && es_div_mod)   ? signed_div_result[63:32]:
+                   (es_div_signed && ~es_div_mod)  ? signed_div_result[31:0]:
+                   (es_div_unsigned && es_div_mod) ? unsigned_div_result[63:32]:
+                   (es_div_unsigned && ~es_div_mod)? unsigned_div_result[31:0] :
+                   es_alu_result;
 
 assign es_alu_src1 = es_src1_is_pc  ? es_pc[31:0] : 
                                       es_rj_value;
@@ -104,6 +156,119 @@ alu u_alu(
     .alu_src2   (es_alu_src2  ),
     .alu_result (es_alu_result)
     );
+
+
+assign unsigned_prod = es_alu_src1 * es_alu_src2;
+assign signed_prod = $signed(es_alu_src1) * $signed(es_alu_src2);
+
+//-------------------------------------------
+div_signed u_div_signed(
+    .aclk                       (clk),
+    .s_axis_divisor_tvalid      (signed_divisor_tvalid),
+    .s_axis_divisor_tready      (signed_divisor_tready),
+    .s_axis_divisor_tdata       (signed_divisor_tdata),
+    .s_axis_dividend_tvalid     (signed_dividend_tvalid),
+    .s_axis_dividend_tready     (signed_dividend_tready),
+    .s_axis_dividend_tdata      (signed_dividend_tdata),
+    .m_axis_dout_tvalid         (signed_dout_tvalid),
+    .m_axis_dout_tdata          (signed_div_result)
+    
+    );
+
+assign signed_divisor_tdata = es_alu_src2;
+assign signed_dividend_tdata = es_alu_src1;
+
+always @(posedge clk)
+begin
+    if(reset) begin
+        signed_divisor_tvalid <= 1'b0;
+        signed_dividend_tvalid <= 1'b0;
+    end
+    else if(es_div_signed & ~signed_divisor_tready & ~signed_dividend_tready & (div_cycle==4'd0) )
+    begin
+        signed_divisor_tvalid <= 1'b1;
+        signed_dividend_tvalid <= 1'b1;
+    end
+    else 
+    begin
+        signed_divisor_tvalid <= 1'b0;
+        signed_dividend_tvalid <= 1'b0;
+    end
+    
+end
+
+div_unsigned u_div_unsigned(
+    .aclk                       (clk),
+    .s_axis_divisor_tvalid      (unsigned_divisor_tvalid),
+    .s_axis_divisor_tready      (unsigned_divisor_tready),
+    .s_axis_divisor_tdata       (unsigned_divisor_tdata),
+    .s_axis_dividend_tvalid     (unsigned_dividend_tvalid),
+    .s_axis_dividend_tready     (unsigned_dividend_tready),
+    .s_axis_dividend_tdata      (unsigned_dividend_tdata),
+    .m_axis_dout_tvalid         (unsigned_dout_tvalid),
+    .m_axis_dout_tdata          (unsigned_div_result)
+    
+    );
+
+assign unsigned_divisor_tdata = es_alu_src2;
+assign unsigned_dividend_tdata = es_alu_src1;
+
+always @(posedge clk)
+begin
+    if(reset) begin
+        unsigned_divisor_tvalid <= 1'b0;
+        unsigned_dividend_tvalid <= 1'b0;
+    end
+    else if(es_div_unsigned & ~unsigned_divisor_tready & ~unsigned_dividend_tready  & (divu_cycle==4'd0))
+    begin
+        unsigned_divisor_tvalid <= 1'b1;
+        unsigned_dividend_tvalid <= 1'b1;
+    end
+    else
+    begin
+        unsigned_divisor_tvalid <= 1'b0;
+        unsigned_dividend_tvalid <= 1'b0;
+    end
+    
+end
+
+always @(posedge clk)
+begin
+    if(reset) begin
+       div_cycle <= 4'd0;
+    end
+    else if(signed_dout_tvalid)
+    begin
+        div_cycle <= 4'd0;
+    end
+    else if (es_div_signed & signed_divisor_tready)
+    begin
+        div_cycle <= div_cycle + 4'd1;
+    end
+    else
+        div_cycle <= div_cycle;
+    
+end
+
+
+always @(posedge clk)
+begin
+    if(reset) begin
+       divu_cycle <= 4'd0;
+    end
+    else if(unsigned_dout_tvalid)
+    begin
+        divu_cycle <= 4'd0;
+    end
+    else if (es_div_unsigned & unsigned_divisor_tready)
+    begin
+        divu_cycle <= divu_cycle + 4'd1;
+    end
+    else
+        divu_cycle <= divu_cycle;
+    
+end
+
 
 assign data_sram_en    = (es_res_from_mem || es_mem_we) && es_valid;
 assign data_sram_wen   = es_mem_we ? 4'hf : 4'h0;
